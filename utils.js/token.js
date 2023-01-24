d71ca61e3e5a7605da458f95621023d9b54894ac
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2023-01-24 23:20:20 +0530

    Add files via upload

--- utils.js/token.js
+++ utils.js/token.js
@@ -1,9 +1,9 @@
 const crypto = require("crypto")
 const db = require("../mongodb")
 
-module.exports = async function (email, type) {
+module.exports = async function (_id, type) {
     let tokenObject = {
-        email: email,
+        Uid: _id,
         token: crypto.randomBytes(32).toString('hex'),
         type,
         created: Date.now(),
