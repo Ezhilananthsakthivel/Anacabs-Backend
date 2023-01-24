ce95c63834fa2d52c57365a071158a8a3f8d82fb
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2023-01-24 23:25:20 +0530

    Update token.js

--- utils.js/token.js
+++ utils.js/token.js
@@ -1,9 +1,9 @@
 const crypto = require("crypto")
 const db = require("../mongodb")
 
-module.exports = async function (_id, type) {
+module.exports = async function (id, type) {
     let tokenObject = {
-        Uid: _id,
+        Uid: id,
         token: crypto.randomBytes(32).toString('hex'),
         type,
         created: Date.now(),
